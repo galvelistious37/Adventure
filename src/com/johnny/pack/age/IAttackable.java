0861d20084486c5d5938ae12210cc326a318037c
galvelistious37 <58580892+galvelistious37@users.noreply.github.com>
2020-04-23 23:18:52 -0700

    Revert "Adventure branch"

---
+++ src/com/johnny/pack/age/IAttackable.java
@@ -0,0 +1,5 @@
+package com.johnny.pack.age;
+
+public interface IAttackable {
+    String attack();
+}
---
+++ src/com/johnny/pack/age/Sting.java
@@ -0,0 +1,8 @@
+package com.johnny.pack.age;
+
+public class Sting implements IAttackable {
+    @Override
+    public String attack() {
+        return "stings";
+    }
+}
