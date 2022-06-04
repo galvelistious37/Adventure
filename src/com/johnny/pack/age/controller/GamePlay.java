9b0317e6061a5ef8735476e171e3ffdee5b056ec
galvelistious37 <galvelistious37@gmail.com>
2022-06-03 23:11:03 -0700

    removing previous update

--- src/com/johnny/pack/age/controller/GamePlay.java
+++ src/com/johnny/pack/age/controller/GamePlay.java
@@ -19,7 +19,6 @@ public class GamePlay {
 
     /**
      * GamePlay Constructor instantiates global variables
-     * ALso testing this new git requirement to use tokens instead of pw
      */
     private GamePlay() {
         playerOne = Player.getInstance();
