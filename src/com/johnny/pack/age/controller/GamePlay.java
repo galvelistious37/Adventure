95b02ab52fb3c808066c73664490af290c47c316
galvelistious37 <galvelistious37@gmail.com>
2022-06-03 22:50:51 -0700

    Refactor GamePlay.java to remove unneccessary code and make everything more readable. Aslo updated Main.java and GamePlayTest.Java with new method names and signatures.

--- src/com/johnny/pack/age/controller/GamePlay.java
+++ src/com/johnny/pack/age/controller/GamePlay.java
@@ -16,145 +16,139 @@ public class GamePlay {
     private Map<Integer, Location> locationMap;
     private List<Character> enemies;
     private Player playerOne;
-    private Fight fightObj;
-    private boolean isStillPlaying;
 
     /**
      * GamePlay Constructor instantiates global variables
      */
     private GamePlay() {
-        fightObj = new Fight();
         playerOne = Player.getInstance();
         locationMap = LocationBuilder.createLocationBuilder().getLocationMap();
         enemies = EnemyBuilder.totalEnemiesList(Constant.TOTAL_ENEMIES).getEnemyList();
-        isStillPlaying = true;
     }
 
+    // Create one and only one instance of GamePlay
     private static final GamePlay INSTANCE = new GamePlay();
 
+    /**
+     * Getter for the one and only instance of the GamePlay
+     * Object.
+     * @return - The GamePlay object instance.
+     */
     public static GamePlay getInstance(){
         return INSTANCE;
     }
 
     /**
-     * Greet and play the game
+     * Greet and initiate the game
      */
-    public void play() {
-        System.out.println(displayGreeting());
+    public void initiate() {
+        System.out.println(createGreeting());
         playTheGame();
     }
 
     /**
      * Print a greeting to the screen
      */
-    public String displayGreeting() {
+    public String createGreeting() {
         return "Welcome to the Greatest Adventure Game Ever!!!";
     }
 
     /**
-     * Main game play method. This method loops through the logic
-     * of each round.
+     * Use a boolean value to determine whether to continue
+     * playing the game
      */
     private void playTheGame(){
+        boolean isStillPlaying = true;
         while (isStillPlaying) {
             isStillPlaying = goThroughPlayActions();
         }
         shutDown();
     }
 
+    /**
+     * This method loops through the logic of game initiate.
+     * @return - A boolean value of whether to keep playing
+     */
     private boolean goThroughPlayActions() {
-        // Get the player location
-        int locationNumber = playerOne.getLocation();
+        // Get the player location int value
+        int locNumber = playerOne.getLocation();
 
         // Determine available exits from the current location
         // and store them in a Map.
         Map<String, Integer> exits;
-        exits = locationMap.get(locationNumber).getExits();
+        exits = locationMap.get(locNumber).getExits();
 
         // Display location details
-        displayLocation(locationNumber);
+        displayLocation(locNumber);
 
-        // If enemies are present in current location, populate
-        // the list of enemies.
-        List<Character> enemies = new ArrayList<>();
-        if(areEnemiesPresent(locationNumber)){
-            enemies = getEnemiesFromLocation(locationNumber);
+        // Determine what to do if enemies are present
+        enemyLogicFlow(locNumber);
 
-            // If enemies are still alive, deal with them.
-            if(fightObj.areEnemiesAlive(enemies)) {
-                dealWithEnemies(enemies);
-            } else {
-                System.out.println("All enemies here are dead");
-            }
-        }
-
-        // If player died dealing with enemies, return false and quit.
+        // If player not alive, return false.
         if(!playerOne.getIsAlive()){
             return false;
         }
 
-
+        // Display the available exits based on current location.
+        // Move to the newly selected location.
         displayAvailableExits(exits);
-        locationNumber = determineMovement(locationNumber, exits);
-        checkNewWeapon(locationNumber);
+        locNumber = moveLocation(locNumber, exits);
+
+        // Check new location for new weapons
+        checkNewWeapon(locNumber);
+
+        // Return true to keep playing.
         return true;
     }
 
-    private int determineMovement(int locationNumber, Map<String, Integer> exits) {
-        String direction = UserInput.getUserInstance().getScanner().nextLine().toUpperCase();
-        int nextLocationNumber = moveInDirection(locationNumber, exits, direction);
-        if (locationNumber != nextLocationNumber) {
-            resetCharacterInitiative(locationNumber);
-            locationNumber = nextLocationNumber;
-        }
-        return locationNumber;
-    }
 
-    private void checkNewWeapon(int locationNumber) {
-        if(locationNumber == Knife.getInstance().getLocation()){
-            setWeaponDetails(Knife.getInstance().weaponType());
-        }
-        if(locationNumber == Sword.getInstance().getLocation()){
-
-            setWeaponDetails(Sword.getInstance().weaponType());
+    /**
+     * Display the details of a given location
+     * @param locationNumber - current location int value
+     */
+    public void displayLocation(int locationNumber) {
+        try{
+            System.out.println("Location: " + locationMap.get(locationNumber).getDescription());
+        } catch (NullPointerException e){
+            throw new NullPointerException();
         }
     }
 
-    private void setWeaponDetails(String weaponType){
-        System.out.println("You found a " + weaponType);
-        playerOne.setEquipable(playerOne.determineEquipable(weaponType));
-        playerOne.setAttackable(playerOne.determineAttackable(playerOne.getEquipable()));
-        playerOne.setBerserkable(playerOne.determineBerserkable(playerOne.getEquipable()));
-        playerOne.setDamage(playerOne.getEquipable().getDamage());
-    }
+    /**
+     * This method progresses through the flow of working
+     * with enemies if they are present.
+     * @param locNumber - current location int value
+     */
+    private void enemyLogicFlow(int locNumber) {
+        // If enemies are present in current location, populate
+        // the list of enemies.
+        List<Character> enemies;
+        if(areEnemiesPresent(locNumber)){
+            enemies = getEnemiesFromLocation(locNumber);
 
-    private void resetCharacterInitiative(int locationNumber){
-        playerOne.setInitiative(0);
-        getEnemiesFromLocation(locationNumber).forEach(enemy -> enemy.setInitiative(0));
-    }
-
-    private int moveInDirection(int locationNumber, Map<String, Integer> exits, String direction) {
-        if(direction.length() > 1){
-            direction = getDirectionFromWord(direction);
+            // If enemies are still alive, deal with them.
+            Fight fight = new Fight();
+            if(fight.areEnemiesAlive(enemies)) {
+                fight.initiative(playerOne, enemies);
+                fight.doFightinStuff(playerOne, enemies);
+            } else {
+                System.out.println("All enemies here are dead");
+            }
         }
-        if(direction.equals("Q")){
-            quit();
-        }
-        if(exits.containsKey(direction)){
-            locationNumber = exits.get(direction);
-            playerOne.setLocation(locationNumber);
-        } else {
-            String wrongDirection = "You cannot go in that direction";
-            System.out.println(wrongDirection);
-        }
-        return locationNumber;
     }
 
-
-    private void dealWithEnemies(List<Character> localEnemies){
-        fightObj.initiative(playerOne, localEnemies);
-        fightObj.doFightinStuff(playerOne, localEnemies);
+    /**
+     * Display the list of available exits.
+     * @param exits - Map of available exits
+     */
+    private void displayAvailableExits(Map<String, Integer> exits) {
+        System.out.println("Available exits are: ");
+        System.out.print("\t");
+        Stream<String> stream = Stream.of(exits.keySet().toString());
+        stream.forEach(System.out::println);
+        System.out.println();
     }
+
 
     /**
      * Find any enemy with the same location as the given parameter
@@ -177,15 +171,57 @@ public class GamePlay {
                 .collect(Collectors.toList());
     }
 
-
-    private void displayAvailableExits(Map<String, Integer> locationExits) {
-        System.out.println("Available exits are: ");
-        System.out.print("\t");
-        Stream<String> stream = Stream.of(locationExits.keySet().toString());
-        stream.forEach(System.out::println);
-        System.out.println();
+    /**
+     * Take user input and determine where to move or to quit.
+     * @param locationNumber - current location int
+     * @param exits - Map of String, Integer exit locations
+     * @return - new location int value
+     */
+    private int moveLocation(int locationNumber, Map<String, Integer> exits) {
+        // Take user input for direction
+        String direction = UserInput.getUserInstance().getScanner().nextLine().toUpperCase();
+        // Get new location int value from input
+        int nextLocationNumber = moveInDirection(locationNumber, exits, direction);
+        // reset initiative and set new location
+        if (locationNumber != nextLocationNumber) {
+            resetCharacterInitiative(locationNumber);
+            locationNumber = nextLocationNumber;
+        }
+        return locationNumber;
     }
 
+    /**
+     * Determine if a location can be updated given the value of user input.
+     * If user input matches a String value of an available exit, update
+     * the location int value to the int value of the new location.
+     * @param locationNumber - current location int value
+     * @param exits - Map of available exits
+     * @param direction - String value user input for chosen direction
+     * @return - the updated int value of the selected new location
+     */
+    private int moveInDirection(int locationNumber, Map<String, Integer> exits, String direction) {
+        if(direction.length() > 1){
+            direction = getDirectionFromWord(direction);
+        }
+        if(direction.equals("Q")){
+            quit();
+        }
+        if(exits.containsKey(direction)){
+            locationNumber = exits.get(direction);
+            playerOne.setLocation(locationNumber);
+        } else {
+            String wrongDirection = "You cannot go in that direction";
+            System.out.println(wrongDirection);
+        }
+        return locationNumber;
+    }
+
+    /**
+     * Break down the String value of words from user input to
+     * determine if they have selected an exit.
+     * @param direction - String value user input
+     * @return - A single character String value used for exit selection
+     */
     private String getDirectionFromWord(String direction) {
         Move moveDirections = new Move();
         String[] words = direction.split(" ");
@@ -197,20 +233,53 @@ public class GamePlay {
         return "X";
     }
 
-    public boolean displayLocation(int locationNumber) {
-        try{
-            System.out.println("Location: " + locationMap.get(locationNumber).getDescription());
-        } catch (NullPointerException e){
-            throw new NullPointerException();
+    /**
+     * Set the initiative for all characters in a given location
+     * to zero.
+     * @param locationNumber - current location int value
+     */
+    private void resetCharacterInitiative(int locationNumber){
+        playerOne.setInitiative(0);
+        getEnemiesFromLocation(locationNumber).forEach(enemy -> enemy.setInitiative(0));
+    }
+
+    /**
+     * WIP progress method: Working on the ability to find and switch
+     * player's set weapon and attacks.
+     * @param locationNumber - current location int value
+     */
+    private void checkNewWeapon(int locationNumber) {
+        if(locationNumber == Knife.getInstance().getLocation()){
+            setWeaponDetails(Knife.getInstance().weaponType());
         }
-        return true;
+        if(locationNumber == Sword.getInstance().getLocation()){
+
+            setWeaponDetails(Sword.getInstance().weaponType());
+        }
     }
 
+    /**
+     * Set player's attacks and damage values based on weapon type
+     * @param weaponType - String value of weapon type
+     */
+    private void setWeaponDetails(String weaponType){
+        System.out.println("You found a " + weaponType);
+        playerOne.setEquipable(playerOne.determineEquipable(weaponType));
+        playerOne.setAttackable(playerOne.determineAttackable(playerOne.getEquipable()));
+        playerOne.setBerserkable(playerOne.determineBerserkable(playerOne.getEquipable()));
+        playerOne.setDamage(playerOne.getEquipable().getDamage());
+    }
 
+    /**
+     * Call to quit the game
+     */
     void quit() {
-        isStillPlaying = false;
+        shutDown();
     }
 
+    /**
+     * Close the open Scanner object and shut down the app
+     */
     private void shutDown(){
         UserInput.getUserInstance().getScanner().close();
         String shutDown = "Shutting down...";
