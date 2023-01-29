35a0dc773a5e0a315b600729d6b59f346a71e247
galvelistious37 <galvelistious37@gmail.com>
2023-01-28 16:25:22 -0800

    Changed project support framework to Maven and updated to Java 12 with preview for switch statements

--- src/com/johnny/pack/age/controller/attack/Berserkable.java
+++
@@ -1,5 +0,0 @@
-package com.johnny.pack.age.controller.attack;
-
-public interface Berserkable {
-    String goBersek();
-}
--- src/com/johnny/pack/age/controller/attack/Equipable.java
+++
@@ -1,6 +0,0 @@
-package com.johnny.pack.age.controller.attack;
-
-public interface Equipable {
-    String weaponType();
-    int getDamage();
-}
